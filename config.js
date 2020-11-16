b0d39f9431fd55081b49e52f68297651c341eae1
mrkunkel <matt@mrkunkel.com>
2020-11-16 11:26:59 -0800

    Updates

--- config.js
+++ config.js
@@ -1,7 +1,5 @@
-// Format: https://api.github.com/repos/{owner}/{repo}/issues/
-const GITHUB_ISSUES_LINK =
-  "https://api.github.com/repos/modulusx/hrc51.com/issues/";
+"use strict";
 
+const GITHUB_ISSUES_LINK = "https://api.github.com/repos/modulusx/hrc51.com/issues/";
 const PATH_SEGMENTS_TO_SKIP = 0;
-
 const HOST = "hrc51.com";
--- redirect.js
+++ redirect.js
@@ -1,52 +1,82 @@
+i"use strict";
+
+function asyncGeneratorStep(gen, resolve, reject, _next, _throw, key, arg) { try { var info = gen[key](arg); var value = info.value; } catch (error) { reject(error); return; } if (info.done) { resolve(value); } else { Promise.resolve(value).then(_next, _throw); } }
+
+function _asyncToGenerator(fn) { return function () { var self = this, args = arguments; return new Promise(function (resolve, reject) { var gen = fn.apply(self, args); function _next(value) { asyncGeneratorStep(gen, resolve, reject, _next, _throw, "next", value); } function _throw(err) { asyncGeneratorStep(gen, resolve, reject, _next, _throw, "throw", err); } _next(undefined); }); }; }
+
 function isUrl(url) {
   // Regex from https://stackoverflow.com/a/3809435
-  return /https?:\/\/(www\.)?[-a-zA-Z0-9@:%._\+~#=]{2,256}\.[a-z]{2,4}\b([-a-zA-Z0-9@:%_\+.~#?&//=]*)/.test(
-    url
-  );
+  return /https?:\/\/(www\.)?[-a-zA-Z0-9@:%._\+~#=]{2,256}\.[a-z]{2,4}\b([-a-zA-Z0-9@:%_\+.~#?&//=]*)/.test(url);
 }
 
-(async function redirect() {
-  const location = window.location;
-  const issueNumber = location.pathname.split("/")[PATH_SEGMENTS_TO_SKIP + 1];
+(function () {
+  var _redirect = _asyncToGenerator( /*#__PURE__*/regeneratorRuntime.mark(function _callee() {
+    var location, issueNumber, homepage, response, payload, message, title, url;
+    return regeneratorRuntime.wrap(function _callee$(_context) {
+      while (1) {
+        switch (_context.prev = _context.next) {
+          case 0:
+            location = window.location;
+            issueNumber = location.pathname.split("/")[PATH_SEGMENTS_TO_SKIP + 1];
+            homepage = location.protocol + "//" + location.hostname + (location.port ? ":" + location.port : "") + "/" + location.pathname.split("/")[PATH_SEGMENTS_TO_SKIP];
+            _context.prev = 3;
+            _context.next = 6;
+            return fetch(GITHUB_ISSUES_LINK + issueNumber);
 
-  const homepage =
-    location.protocol +
-    "//" +
-    location.hostname +
-    (location.port ? ":" + location.port : "") +
-    "/" +
-    location.pathname.split("/")[PATH_SEGMENTS_TO_SKIP];
+          case 6:
+            response = _context.sent;
 
-  try {
-    const response = await fetch(GITHUB_ISSUES_LINK + issueNumber);
+            if (!(response.status !== 200)) {
+              _context.next = 10;
+              break;
+            }
 
-    if (response.status !== 200) {
-      location.replace(homepage);
-      return;
-    }
+            location.replace(homepage);
+            return _context.abrupt("return");
 
-    const payload = await response.json();
-    let { message, title } = payload;
+          case 10:
+            _context.next = 12;
+            return response.json();
 
-    if (message === "Not Found") {
-      // issueNumber does not exist in gh issues
-      location.replace(homepage);
-    } else if (!title || !isUrl(title)) {
-      location.replace(homepage);
-    } else {
-      const url = new URL(title);
+          case 12:
+            payload = _context.sent;
+            message = payload.message, title = payload.title;
 
-      if (
-        (url.protocol !== "https:" && url.protocol !== "http:") ||
-        url.host === HOST
-      ) {
-        // Prevent recursive redirects and XSS
-        location.replace(homepage);
-      } else {
-        location.replace(title);
+            if (message === "Not Found") {
+              // issueNumber does not exist in gh issues
+              location.replace(homepage);
+            } else if (!title || !isUrl(title)) {
+              location.replace(homepage);
+            } else {
+              url = new URL(title);
+
+              if (url.protocol !== "https:" && url.protocol !== "http:" || url.host === HOST) {
+                // Prevent recursive redirects and XSS
+                location.replace(homepage);
+              } else {
+                location.replace(title);
+              }
+            }
+
+            _context.next = 20;
+            break;
+
+          case 17:
+            _context.prev = 17;
+            _context.t0 = _context["catch"](3);
+            location.replace(homepage);
+
+          case 20:
+          case "end":
+            return _context.stop();
+        }
       }
-    }
-  } catch (e) {
-    location.replace(homepage);
+    }, _callee, null, [[3, 17]]);
+  }));
+
+  function redirect() {
+    return _redirect.apply(this, arguments);
   }
-})();
+
+  return redirect;
+})()();
